2d86bc16299321984d51c6f72bafa9947f224acf
Ottavio42 <ottaviog88@gmail.com>
2021-09-17 03:59:17 +0200

    fine-esercizio

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+const application = new Vue({
+
+    el: "#application",
+    data:{
+        message: "Samuele Madrigali",
+        formatting:["center-formatting", "h1-formatting"],
+        image:{
+            imageUrl: "https://secure.gravatar.com/avatar/f59854b4816b398cb25a7105478b7d2d?s=180&d=identicon",
+            imageDescription: "Samuele"
+        }
+    }
+
+});
